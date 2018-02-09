a5ac4029f2560fdd7c0a80ebc3e85b75cda57796
Troy Lawrence <redirect@gmail.com>
2018-02-09 00:58:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,22 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  var itemPrice = Math.floor((Math.random() * 100) + 1);
+  var item = { [itemName]: itemPrice };
+ cart.push(item);
+ console.log(`${itemName} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart.length>0) {
+      return `In your shopping cart you have ${cart
+    } else {
+      return `Your shopping cart is empty.`;
+    }
+  }
 }
 
 function total() {
